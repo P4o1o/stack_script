1153a0cdbbf3308b42afc0537335fcd9df73d147
P4o1o <86971195+P4o1o@users.noreply.github.com>
2024-05-19 23:49:30 +0200

    more examples

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@ int main() {
     struct ProgramState state = init_PrgState(256, 256);
     struct ExceptionHandler *try_buf = malloc(sizeof(struct ExceptionHandler));
     TRY(try_buf){
-        execute(&state, "[size swap(size 1 -) [+] loop(size 2 >) swap /] define(mean)", try_buf);
+        execute(&state, "[size [[+] loop(size 1 >)] dip /] define(mean)", try_buf);
         execute(&state, "[[swap(size 1 -) 1 + dup] [< swap swap(size 1 -) swap] swap3 quote compose swap quote swap2 compose compose loop swap(size 1 -) drop] define(fill)", try_buf);
         execute(&state, "0 10 30 fill 10023 [mean] 100 90 dig2 apply printall", try_buf);
     }CATCHALL{
