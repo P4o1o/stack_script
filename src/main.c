d7f7084b2628233169a9fa1d6c609269468e98f3
P4o1o <86971195+P4o1o@users.noreply.github.com>
2024-05-20 23:18:07 +0200

    shell

--- src/main.c
+++ src/main.c
@@ -1,16 +1,24 @@
 #include <stdio.h>
 #include "interpreter.h"
 #include "buildutils.h"
+#define BUFFERSIZE 256
 
 int main() {
     struct ProgramState state = init_PrgState(256, 256);
-    struct ExceptionHandler *try_buf = malloc(sizeof(struct ExceptionHandler));
-    TRY(try_buf){
-        execute(&state, "[size [[+] loop(size 1 >)] dip /] define(mean)", try_buf);
-        execute(&state, "[[swap(size 1 -) 1 + dup] [< swap swap(size 1 -) swap] swap3 quote compose swap quote swap2 compose compose loop swap(size 1 -) drop] define(fill)", try_buf);
-        execute(&state, "0 10 30 fill 10023 [mean] 100 90 dig2 apply printall", try_buf);
-    }CATCHALL{
-        printf("Exception number %d\n", try_buf->exit_value);
+    struct ExceptionHandler* try_buf = malloc(sizeof(struct ExceptionHandler));
+    char* bufferin[BUFFERSIZE];
+    while(1){
+        printf(">");
+        fflush(stdout);
+        TRY(try_buf) {
+            if (fgets(bufferin, BUFFERSIZE, stdin) >= 0) {
+                execute(&state, bufferin, try_buf);
+            }
+        }CATCH(try_buf, ProgramExit) {
+            break;
+        }CATCHALL{
+            printf("Exception number %d\n", try_buf->exit_value);
+        }
     }
     free(try_buf);
     free_PrgState(&state);
